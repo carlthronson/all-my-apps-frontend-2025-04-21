600b1678522b74aefde53bed5da6efed94074e52
Carl Thronson <carlthronson@gmail.com>
2025-05-12 11:50:15 -0700

    Protect calendar page, force user to login

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,7 +1,16 @@
-'use client';
+// 'use client';
 import Calendar from '@/components/Calendar';
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/auth.config";
+import { redirect } from "next/navigation";
 
-export default function CalendarPage() {
+export default async function CalendarPage() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/login");
+  }
+
   return <Calendar />;
 }
 
