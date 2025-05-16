a62be42a7f2749144041bc69d316765ab0d05922
Carl Thronson <carlthronson@gmail.com>
2025-05-16 15:34:44 -0700

    Revert "Don't show the columns for "No" and "Unlikely""
    This reverts commit 78e10b21bd3974f893f8c3033489a25dbcfcb124.

--- src/components/StoryBoard.jsx
+++ src/components/StoryBoard.jsx
@@ -3,20 +3,23 @@
 import PhaseColumn from './PhaseColumn';
 import { useJobSearch } from '@/contexts/JobSearchContext';
 
+// const StoryBoardArea = styled.div`
+//   display: flex;
+//   flex-direction: row;
+// `;
+
 export default function StoryBoard() {
   const { phases } = useJobSearch();
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', gap: 8 }}>
-      {phases
-        .filter(phase => phase.label !== "No" && phase.label !== "Unlikely")
-        .map((phase, index) => (
-          <PhaseColumn
-            key={index}
-            id={index}
-            phase={phase}
-          />
-        ))}
+      {phases.map((phase, index) => (
+        <PhaseColumn
+          key={index}
+          id={index}
+          phase={phase}
+        />
+      ))}
     </div>
   );
 }
